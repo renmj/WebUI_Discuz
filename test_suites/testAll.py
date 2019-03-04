7bdfd2882f81b72b1de9242ddf01f1515103f4c6
Grace15034064152 <1130489267@qq.com>
2019-03-04 23:26:24 +0800

    first commit

--- test_suites/testAll.py
+++ test_suites/testAll.py
@@ -1,6 +1,11 @@
-import unittest
 import os
+import sys
+import unittest
 import HTMLTestRunner
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+sys.path.append("E:\\pythonWorkplace\\UITest")
 test_dir="./"
 suite=unittest.TestLoader().discover(test_dir,pattern='test_*.py')
 #设置报告路径
